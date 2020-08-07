7c137ed9b91a3867c2ac657ae3b1f35105cf6afa
Vadim Shalugin <vd.shalugin@gmail.com>
2020-08-07 19:29:35 +0200

    Initial Commit

---
+++ NASALibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NASALibrary
+//
+//  Created by Vadim Shalugin on 07.08.2020.
+//  Copyright © 2020 Vadim Shalugin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
